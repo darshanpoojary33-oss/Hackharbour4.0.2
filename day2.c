69d7cb491ad9ec954111c1c5ffbc4a7feab73014
Darshan <darshanpoojary33@gmail.com>
2026-08-19 14:22:49 +0530

    my first commit

---
+++ day2.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+ #include<stdlib.h>
+ int main()
+ {
+    int a;
+    printf("enter the value od a :");
+    scanf("%d",&a):
+    printf("The value is:%d",a);
+    return 0;
+ }
